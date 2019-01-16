b9a103838c63671c9c008e33bf75dcfcfebf7452
Troy Hicks <NOLAWolfe@users.noreply.github.com>
2019-01-16 11:06:36 -0500

    morning error fixing

--- Krowd/src/main/java/com/krowd/main/Driver.java
+++ Krowd/src/main/java/com/krowd/main/Driver.java
@@ -1,13 +1,8 @@
 package com.krowd.main;
 
-import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-import com.krowd.beans.Users;
-import com.krowd.dao.JunctionDAO;
-import com.krowd.dao.JunctionDAOImpl;
 import com.krowd.dao.UserDAO;
 import com.krowd.dao.UserDAOImpl;
 import com.krowd.util.HibernateUtil;
@@ -20,58 +15,11 @@ public class Driver {
 
 		SessionFactory sf = HibernateUtil.getSessionFactory();
 //		testingUsers(sf); // pretty self explanatory
-		junctionTableTest(sf);
 	}
-	
-	static void junctionTableTest(SessionFactory sf) {
-		Session s = sf.getCurrentSession();
-		JunctionDAO jd = new JunctionDAOImpl();
-		
-		System.out.println(jd.getAll());
-	}
-
 	static void testingUsers(SessionFactory sf) {
-		JunctionDAO jd = new JunctionDAOImpl();
 		UserDAO ud = new UserDAOImpl();
 		Session s = sf.getCurrentSession();
-
-		
-//		System.out.println(jd.getAttendingUsersList(10));
-
-		// IM A MADDDDDDD-MAN
-
-//		String[] array = new String[] { "https://randomuser.me/api/portraits/women/85.jpg",
-//				"https://randomuser.me/api/portraits/women/10.jpg", "https://randomuser.me/api/portraits/men/65.jpg",
-//				"https://randomuser.me/api/portraits/women/10.jpg", "https://randomuser.me/api/portraits/men/78.jpg",
-//				"https://randomuser.me/api/portraits/women/42.jpg", "https://randomuser.me/api/portraits/men/39.jpg",
-//				"https://randomuser.me/api/portraits/women/77.jpg", "https://randomuser.me/api/portraits/men/34.jpg",
-//				"https://randomuser.me/api/portraits/women/55.jpg", "https://randomuser.me/api/portraits/women/4.jpg",
-//				"https://randomuser.me/api/portraits/men/27.jpg", "https://randomuser.me/api/portraits/women/54.jpg",
-//				"https://randomuser.me/api/portraits/men/50.jpg", "https://randomuser.me/api/portraits/women/22.jpg",
-//				"https://randomuser.me/api/portraits/men/63.jpg", "https://randomuser.me/api/portraits/women/83.jpg",
-//				"https://randomuser.me/api/portraits/men/36.jpg", "https://randomuser.me/api/portraits/women/2.jpg",
-//				"https://randomuser.me/api/portraits/women/88.jpg", "https://randomuser.me/api/portraits/men/2.jpg",
-//				"https://randomuser.me/api/portraits/women/64.jpg", "https://randomuser.me/api/portraits/men/68.jpg",
-//				"https://randomuser.me/api/portraits/women/11.jpg", "https://randomuser.me/api/portraits/men/66.jpg",
-//				"https://randomuser.me/api/portraits/women/32.jpg", "https://randomuser.me/api/portraits/men/59.jpg"};
-
-		int counter = 0;
-		List<Users> ls = ud.getAllUsers();
-
-//		for (Users u : ls) {
-//			
-//			u.setPhoto_url(array[counter]);
-//			ud.updateUsers(u);
-//		
-//			System.out.println(u);
-//			counter ++;
-//			
-//			if(counter == array.length) {
-//				counter = 0;
-//			}
-//			
-//		}
-
+		System.out.println(ud.getAllUsers());
 		s.close();
 	}
 
