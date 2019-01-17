76f9f09d999d5e879f1360173162d974dcd52c00
unknown <cbejar93@gmail.com>
2019-01-16 20:18:08 -0500

    routeguards

--- KrowdClient/src/app/landing/auth/auth.service.ts
+++ KrowdClient/src/app/landing/auth/auth.service.ts
@@ -4,12 +4,20 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import * as firebase from 'firebase';
+import { tokenKey } from '@angular/core/src/view';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  tk: string; 
+  firebaseID: string;
+  currentUser:{
+    ctk: string,
+    cUserID: number
+  }
 
   httpHeaders = new HttpHeaders({
     'Content-Type': 'application/json',
@@ -39,7 +47,13 @@ export class AuthService {
         response=>{
           console.log(response);
           let fID: string= response.user.uid;
-          this.signInUser(username, password, email, fID)
+          this.signInUser(username, password, email, fID);
+          firebase.auth().currentUser.getIdToken().then(
+             (token:string)=>
+              this.tk= token
+
+
+          )
         }
       )
       .catch(error=>console.log(error))
@@ -49,22 +63,23 @@ export class AuthService {
 
     this.http.post('http://localhost:8080/Krowd/login/sent', { 'username': username, 'password': password, 'email': email, 'fID': fID })
       .subscribe((event) => this.router.navigate(['/home']));
+  }
 
+  getToken(){
+    firebase.auth().currentUser.getIdToken().then(
+      (token:string)=>
+       this.tk= token
 
-
-
-
+   )
   }
 
   isAuthenticated() {
-    // return this.user != null;
-
-    return false;
+    return this.currentUser.ctk != null;  
   }
 
   logOut() {
-
-
-
+    firebase.auth().signOut;
+    this.tk = null;
   }
+
 }
--- KrowdClient/src/app/navbar/navbar.component.ts
+++ KrowdClient/src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
+import { AuthService } from '../landing/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -19,12 +20,15 @@ export class NavbarComponent implements OnInit {
     }
   }
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
   }
+
+
   onLogout(){
     console.log("hellot");
     this.router.navigate(['/landing']);
+    this.authService.logOut();
   }
 }
