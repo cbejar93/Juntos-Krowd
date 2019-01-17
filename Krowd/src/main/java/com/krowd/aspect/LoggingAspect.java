aa42df95cda4b2767a3f5c9cb51497492e65ca28
Joseph Guzman <jguzman54221@gmail.com>
2019-01-16 20:58:28 -0500

    Complete logging

--- Krowd/src/main/java/com/krowd/aspect/LoggingAspect.java
+++ Krowd/src/main/java/com/krowd/aspect/LoggingAspect.java
@@ -2,6 +2,8 @@ package com.krowd.aspect;
 
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -20,13 +22,13 @@ public class LoggingAspect {
 	/*
 	 * Declaring the logging variable to be able to print out our logs to the log.txt file
 	 */
-	private static Logger log = Logger.getLogger(LoggingAspect.class);
+	private static final Logger log = Logger.getLogger(LoggingAspect.class);
 	
 	/*
 	 * Making a general PointCut where the advice can be applied. This applies to all the methods and 
 	 * their executions. This makes it easier for logging.
 	 */
-	@Pointcut("execution(*com.krowd.*(..)")
+	@Pointcut("execution(* com.krowd.*(..)")
 	private void generalPointCut() {
 		
 	}
@@ -37,9 +39,19 @@ public class LoggingAspect {
 	 */
 	@Before("generalPointCut()")
 	public void infoLog(JoinPoint jp) {
+		System.out.println("Before ");
 		log.info(jp.getTarget().getClass().getSimpleName()+" : "+jp.getSignature().getName());
 	}
 	
+	/*
+	 * This method is called AFTER the method that is being called is executed and logged. This method will
+	 * show the word "After" and give the name of the class and method being executed.
+	 */
+	@After("generalPointCut()")
+	public void afterInfoLog(JoinPoint jp) {
+		System.out.println("After ");
+		log.info(jp.getTarget().getClass().getSimpleName()+" : "+jp.getSignature().getName());
+	}
 	
 
 }
